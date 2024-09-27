a471f8e8e8a8dedc73e02f940c3ba7fca6d638f0
Savio Anjos <savioanjos2020@gmail.com>
2024-09-27 16:27:39 -0300

    feat: :sparkles: Added theme configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  return (
+    <div className={styles.page}>
+      <h1>Test</h1>
+    </div>
+  );
 }
