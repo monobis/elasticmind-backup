4f484df1b353113b38eed155119f3b6f677e689a
Savio Anjos <savioanjos2020@gmail.com>
2024-10-27 12:46:59 -0300

    feat: :sparkles: Added articles core content

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -7,7 +7,22 @@ export default function Articles() {
     <main>
       <Header />
 
-      <section className={styles.container}></section>
+      <section className={styles.container}>
+        <div className={styles.coreContent}>
+          <h1 className={styles.coreTitle}>
+            <span className={styles.titleCategory}>Título:</span> Chamada
+            notícia Principal.
+          </h1>
+
+          <p className={styles.coreDescription}>
+            Elasticmind is more than just a software development company. We’re
+            a team of people driven by innovation who pull off the impossible
+            with the blend of the brightest technological minds.
+          </p>
+
+          <button className={styles.buttonReadMore}>Read more</button>
+        </div>
+      </section>
 
       <div className={styles.footer}>
         {" "}
