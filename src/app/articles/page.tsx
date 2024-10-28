696bf864a0d78a9aaf41a8bc1665a4378e46cbdd
Savio Anjos <savioanjos2020@gmail.com>
2024-10-28 00:25:28 -0300

    feat: :sparkles: Added CardArticle Componenet

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import styles from "./page.module.css";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { CardArticle } from "@/components/CardArticle";
+import ArticleImage from "@/../public/immage-tecnology.jpg";
 
 export default function Articles() {
   const [selectedCategory, setSelectedCategory] = useState("ALL ARTICLE");
@@ -64,6 +66,20 @@ export default function Articles() {
               consectetur, adipisci velit…”
             </p>
           </header>
+
+          <section className={styles.articles}>
+            <CardArticle
+              image={ArticleImage}
+              sector="nft"
+              title="Neque porro quisquam est qui dolorem"
+              description="Lorem ipsum dolor sit amet, consectetur adipiscing 
+                        elit. Pellentesque tempus massa ac felis gravida 
+                        eleifend."
+              author="John Doe"
+              authorPosition="CEO"
+              category="allArticles"
+            />
+          </section>
         </section>
       </section>
 
