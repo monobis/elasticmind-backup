d6c8e26a5a6854883c5020d4a696807a80989d13
Savio Anjos <savioanjos2020@gmail.com>
2024-10-29 10:02:08 -0300

    feat: :sparkles: Configured ArticleDetails page

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -25,6 +25,59 @@ adipisci velit…”"
         />
       </section>
 
+      <section className={styles.articleContent}>
+        <p className={styles.articleParagraph}>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          tempus massa ac felis gravida eleifend. Suspendisse mattis rhoncus
+          purus, et commodo mi lacinia at. Aenean auctor et felis id feugiat.
+          Nulla facilisi. Maecenas auctor augue quis justo pretium sodales.
+          Proin in tincidunt felis. Nunc fringilla tellus turpis, in accumsan
+          tellus blandit sed. Ut sit amet imperdiet ipsum. Donec varius et dui a
+          dignissim. Vivamus id metus dapibus, auctor risus non, venenatis odio.
+          Nunc ultricies facilisis ligula ac congue. Mauris lectus odio,
+          accumsan quis gravida non, maximus vitae nisi. Nunc vel erat
+          fermentum, accumsan augue vel, eleifend tellus. Vestibulum a ex nec
+          ipsum aliquam commodo. Donec arcu orci, mollis sit amet blandit at,
+          ultrices ornare diam. Mauris at lacus tincidunt, dignissim velit et,
+          facilisis velit.
+        </p>
+
+        <p className={styles.articleParagraph}>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          tempus massa ac felis gravida eleifend. Suspendisse mattis rhoncus
+          purus, et commodo mi lacinia at. Aenean auctor et felis id feugiat.
+          Nulla facilisi. Maecenas auctor augue quis justo pretium sodales.
+          Proin in tincidunt felis. Nunc fringilla tellus turpis, in accumsan
+          tellus blandit sed. Ut sit amet imperdiet ipsum. Donec varius et dui a
+          dignissim. Vivamus id metus dapibus, auctor risus non, venenatis odio.
+          Nunc ultricies facilisis ligula ac congue. Mauris lectus odio,
+          accumsan quis gravida non, maximus vitae nisi. Nunc vel erat
+          fermentum, accumsan augue vel, eleifend tellus. Vestibulum a ex nec
+          ipsum aliquam commodo. Donec arcu orci, mollis sit amet blandit at,
+          ultrices ornare diam. Mauris at lacus tincidunt, dignissim velit et,
+          facilisis velit.
+        </p>
+
+        <p className={styles.articleParagraph}>
+          {" "}
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque
+          tempus massa ac felis gravida eleifend. Suspendisse mattis rhoncus
+          purus, et commodo mi lacinia at. Aenean auctor et felis id feugiat.
+          Nulla facilisi. Maecenas auctor augue quis justo pretium sodales.
+          Proin in tincidunt felis. Nunc fringilla tellus turpis, in accumsan
+          tellus blandit sed. Ut sit amet imperdiet ipsum. Donec varius et dui a
+          dignissim. Vivamus id metus dapibus, auctor risus non, venenatis odio.
+          Nunc ultricies facilisis ligula ac congue. Mauris lectus odio,
+          accumsan quis gravida non, maximus vitae nisi. Nunc vel erat
+          fermentum, accumsan augue vel, eleifend tellus. Vestibulum a ex nec
+          ipsum aliquam commodo. Donec arcu orci, mollis sit amet blandit at,
+          ultrices ornare diam. Mauris at lacus tincidunt, dignissim velit et,
+          facilisis velit.
+        </p>
+      </section>
+
       <div className={styles.footer}>
         {" "}
         <Footer />
